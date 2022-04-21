0ec72e8b978097c0bd783256e9fc14a121c8950b
Silvia-Du <silviadu@hotmail.it>
2022-04-21 17:33:22 +0200

    aggiunto aumento dei box al cambiare della scelta utente

--- js/script.js
+++ js/script.js
@@ -11,45 +11,58 @@ const gameArea = document.getElementById('sd-game-area');
 //mi serve un bottone che raccolga i dati della scelta utente
 
 document.getElementById('btn-start').addEventListener('click', function(){
-  let numberBox;
+  let numberBox, size;
   const levelChoice = document.getElementById('level-choice').value;
 
   if(levelChoice === 'Easy'){
     numberBox = 49;
+    size = 1;
+
   }else if(levelChoice === 'Hard'){
     numberBox = 81;
+    size = 2;
+
   }else{
     numberBox = 100;
+    size = 3;
+
   }
 
-initGame(numberBox);
+initGame(numberBox, gameArea, size );
 })
 
 
 //function
 
 //funzione che stampa in pagina tot box
-function initGame(parametroN){
+function initGame(parametroN, location, parameterSize){
 
   for( let i = 0; i < parametroN; i++ ){
 
-    const boxEr = createBoxElement( gameArea );
+    const boxEr = createBoxElement( location, parameterSize );
     boxEr.addEventListener('click', function(){
       this.classList.add('check');
     })
-
   }
-
 }
 
 
 //funzione che crea l'elemento e lo appende in un contenitore
-function createBoxElement(container){
+function createBoxElement(container, parameter){
 
   const box = document.createElement('div');
   box.className = 'sd-box';
-  box.classList.add('easy');
   container.append(box);
+
+  if( parameter === 1 ){
+    box.classList.add('easy');
+
+  }else if( parameter === 2 ){
+    box.classList.add('hard');
+
+  }else{
+    box.classList.add('crazy');
+  }
 
   return box;
 }
