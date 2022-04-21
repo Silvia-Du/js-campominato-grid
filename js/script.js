3c85696d84629a9ab79db28b15934ef7b90852b0
Silvia-Du <silviadu@hotmail.it>
2022-04-21 16:55:34 +0200

    fine test di stampa box con un solo livello di difficoltà

--- js/script.js
+++ js/script.js
@@ -6,11 +6,45 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
+//prendo il conteniroe dei box
+const gameArea = document.getElementById('sd-game-area');
 //mi serve un bottone che raccolga i dati della scelta utente
 
 document.getElementById('btn-start').addEventListener('click', function(){
-
+  let numberBox;
   const levelChoice = document.getElementById('level-choice').value;
 
-  console.log(levelChoice);
+  if(levelChoice === 'Easy'){
+    numberBox = 49;
+  }else if(levelChoice === 'Hard'){
+    numberBox = 81;
+  }else{
+    numberBox = 100;
+  }
+
+initGame(numberBox);
 })
+
+
+//function
+
+//funzione che stampa in pagina tot box
+function initGame(parametroN){
+
+  for( let i = 0; i < parametroN; i++ ){
+    createBoxElement(gameArea);
+  }
+
+}
+
+
+//funzione che crea l'elemento e lo appende in un contenitore
+function createBoxElement(container){
+
+  const box = document.createElement('div');
+  box.className = 'sd-box';
+  box.classList.add('easy');
+  container.append(box);
+
+  return box;
+}
